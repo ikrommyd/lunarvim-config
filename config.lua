abf41c33be5b3a12a60b012cc3ea190e114c9288
Iason Krommydas <iason.krom@gmail.com>
2025-04-11 00:36:52 -0500

    add telescope and tokyonight

--- config.lua
+++ config.lua
@@ -5,7 +5,6 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 --
 vim.opt.relativenumber = true
-vim.opt.guicursor = ""
 
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
@@ -25,6 +24,35 @@ lvim.plugins = {
 			})
 		end,
 	},
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            require("tokyonight").setup({
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                transparent = false, -- Enable this to disable setting the background color
+                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                styles = {
+                    -- Style to be applied to different syntax groups
+                    -- Value is any valid attr-list value for `:help nvim_set_hl`
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    -- Background styles. Can be "dark", "transparent" or "normal"
+                    sidebars = "dark", -- style for sidebars, see below
+                    floats = "dark", -- style for floating windows
+                },
+            })
+        end
+    },
+  {
+    "nvim-telescope/telescope.nvim",
+      dependencies = {
+        { "nvim-telescope/telescope-live-grep-args.nvim" },
+      },
+      config = function()
+        require("telescope").load_extension("live_grep_args")
+      end
+  }
 }
-lvim.colorscheme = "lunar"
 lvim.builtin.lualine.style = "lvim"
